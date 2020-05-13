9903a1684d16f6b37332919b7eec86fe9a5bfbd7
GVRO1 <gabriel.oliveira@bandte.com.br>
2020-05-13 18:26:36 -0300

    commit:deletando o site antigo e subindo o novo.

---
+++ site_oficial_sprint/js/script.js
@@ -0,0 +1,90 @@
+function trocar2() {
+
+
+
+    if (tree3.checked == true) {
+
+
+        imagetre.src = "imagens/perfil.png";
+        texto.innerHTML = `<span>Alexandre Diogo<span>`;
+
+
+
+
+    } else if (tree1.checked == true) {
+
+
+        imagetre.src = "imagens/perfil2.png";
+        texto.innerHTML = `<span>Cauê Pontes<span>`;
+
+
+    } else if (tree2.checked == true) {
+
+
+        imagetre.src = "imagens/perfil3.png";
+        texto.innerHTML = `<span>Isabela Victoria<span>`;
+
+
+
+    } else {
+        alert("não foi pssivel carregar algumas imagens")
+    }
+}
+
+function trocar() {
+
+
+
+    if (one3.checked == true) {
+
+
+        imgone.src = "imagens/perfil.png";
+        texto1.innerHTML = `<span>Mathias Carvalho<span>`;
+
+
+
+
+    } else if (one1.checked == true) {
+
+
+        imgone.src = "imagens/perfil2.png";
+        texto1.innerHTML = `<span>Leonardo Victor<span>`;
+
+
+
+    } else if (one2.checked == true) {
+
+
+        imgone.src = "imagens/perfil3.png";
+        texto1.innerHTML = `<span>Mathueus Juan<span>`;
+
+
+
+    } else {
+        alert("não foi pssivel carregar algumas imagens")
+    }
+
+}
+
+
+function mover() {
+    if (bIndicador.innerHTML == "Ver Video") {
+        Descvideo.style.marginRight = "0.5%";
+        videozin.style.display = "block"
+        bIndicador.innerHTML = `Ocultar Video`;
+
+
+    } else if (bIndicador.innerHTML == "Ocultar Video") {
+
+        videozin.style.display = "none"
+
+        bIndicador.innerHTML = `Ver Video`;
+        Descvideo.style.marginRight = "25%";
+
+
+    } else {
+        alert("nao foi possivel visualizar o video");
+    }
+
+
+}
