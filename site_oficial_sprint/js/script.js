f1396b882e112c896d9ffd318d612cc963eeda3a
GVRO1 <gabriel.oliveira@bandte.com.br>
2020-05-21 12:48:37 -0300

    commit: parte 1 da correção da aba lateral.

--- site_oficial_sprint/js/script.js
+++ site_oficial_sprint/js/script.js
@@ -88,14 +88,21 @@ function mover() {
 
     
 }
+
 function abrir_menu(x){
+    console.log(x);
     x.classList.toggle("change");
-    document.getElementById("navLateral").style.width = "250px";
+    document.getElementById("div_transform").style.width = "270px";
+    document.getElementById("div_transform").style.marginLeft = "270px";
+    document.getElementById("nav_lado").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
   document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
+
 function fecharNav(){
-    document.getElementById("navLateral").style.width = "0";
+    document.getElementById("div_transform").style.width ="0";
+    document.getElementById("div_transform").style.marginLeft ="0";
+    document.getElementById("nav_lado").style.width = "0";
   document.getElementById("main").style.marginLeft= "0";
   document.body.style.backgroundColor = "white";
 }
