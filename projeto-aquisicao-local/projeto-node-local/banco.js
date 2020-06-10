6cbbc6d4e6aecb1b504335fe2b81f54247383dd3
GVRO1 <gabriel.oliveira@bandte.com.br>
2020-06-10 16:54:52 -0300

    subindo os sensor-virtuais e a documentacao

---
+++ projeto-aquisicao-local/projeto-node-local/banco.js
@@ -0,0 +1,33 @@
+const configuracoes = {
+    banco: {
+        server: "servercampfiresolutions.database.windows.net",
+        user: "adminbandtec",
+        password: "#Gfcampfire",
+        database: "BdCampFire",
+        options: {
+            encrypt: true
+        },
+        pool: {
+            max: 4,
+            min: 1,
+            idleTimeoutMillis: 30000,
+            connectionTimeout: 5000
+        }
+    }
+}
+ 
+const sql = require('mssql');
+sql.on('error', err => {
+    console.error(`Erro de Conexão: ${err}`);
+});
+
+
+function conectar() {
+  sql.close();
+  return sql.connect(configuracoes.banco)
+} 
+
+module.exports = {
+    conectar: conectar,
+    sql: sql
+}
