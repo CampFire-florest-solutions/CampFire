14f715c63df054bf048e3488a7a6d038b47035e3
GVRO1 <gabriel.oliveira@bandte.com.br>
2020-06-30 10:11:28 -0300

    site finalizado

--- projeto-site/models/usuario.js
+++ projeto-site/models/usuario.js
@@ -6,35 +6,39 @@ https://codewithhugo.com/sequelize-data-types-a-practical-guide/
 */
 
 module.exports = (sequelize, DataTypes) => {
-    let Usuario = sequelize.define('Usuario',{
-		id: {
-			field: 'id',
-			type: DataTypes.INTEGER,
-			primaryKey: true,
-			autoIncrement: true
-		},		
-		nome: {
-			field: 'nome',
-			type: DataTypes.STRING,
-			allowNull: false
-		},
-		login: {
-			field: 'login',
-			type: DataTypes.STRING,
-			allowNull: false
-		},
-		senha: {
-			field: 'senha',
-			type: DataTypes.STRING,
-			allowNull: false
-		}
-	}, 
-	{
-		tableName: 'usuario', 
-		freezeTableName: true, 
-		underscored: true,
-		timestamps: false,
-	});
+    let Usuario = sequelize.define('Usuario', {
+        id: {
+            field: 'id',
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        nome: {
+            field: 'nomes',
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        login: {
+            field: 'logins',
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        senha: {
+            field: 'senhas',
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        codigo: {
+            field: 'codigo',
+            type: DataTypes.INTEGER,
+            allowNull: false
+        }
+    }, {
+        tableName: 'usuarios',
+        freezeTableName: true,
+        underscored: true,
+        timestamps: false,
+    });
 
     return Usuario;
 };
--- projeto-site/routes/leituras.js
+++ projeto-site/routes/leituras.js
@@ -5,74 +5,74 @@ var Leitura = require('../models').Leitura;
 
 /* Recuperar as últimas N leituras */
 router.get('/ultimas', function(req, res, next) {
-	
-	// quantas são as últimas leituras que quer? 8 está bom?
-	const limite_linhas = 7;
 
-	console.log(`Recuperando as últimas ${limite_linhas} Eventos`);
-	
-	const instrucaoSql = `select top ${limite_linhas} 
-						Temperatura, 
-						Umidade, 
-						Momento,
-						FORMAT(Momento,'HH:mm:ss') as momento_grafico 
-						from Eventos order by ID_Evento desc`;
+    // quantas são as últimas leituras que quer? 8 está bom?
+    const limite_linhas = 7;
 
-	sequelize.query(instrucaoSql, {
-		model: Leitura,
-		mapToModel: true 
-	  })
-	  .then(resultado => {
-			console.log(`Encontrados: ${resultado.length}`);
-			res.json(resultado);
-	  }).catch(erro => {
-			console.error(erro);
-			res.status(500).send(erro.message);
-	  });
+    console.log(`Recuperando as últimas ${limite_linhas} leituras`);
+
+    const instrucaoSql = `select top ${limite_linhas} 
+						temperatura, 
+						umidade, 
+						momento,
+						FORMAT(momento,'HH:mm:ss') as momento_grafico 
+						from Eventos order by id_evento desc`;
+
+    sequelize.query(instrucaoSql, {
+            model: Leitura,
+            mapToModel: true
+        })
+        .then(resultado => {
+            console.log(`Encontrados: ${resultado.length}`);
+            res.json(resultado);
+        }).catch(erro => {
+            console.error(erro);
+            res.status(500).send(erro.message);
+        });
 });
 
 
 // tempo real (último valor de cada leitura)
-router.get('/tempo-real', function (req, res, next) {
-	
-	console.log(`Recuperando a última leitura`);
+router.get('/tempo-real', function(req, res, next) {
 
-	const instrucaoSql = `select top 1 Temperatura, Umidade, FORMAT(Momento,'HH:mm:ss') as momento_grafico  
-						from Eventos order by ID_Evento desc`;
+    console.log(`Recuperando a última leitura`);
 
-	sequelize.query(instrucaoSql, { type: sequelize.QueryTypes.SELECT })
-		.then(resultado => {
-			res.json(resultado[0]);
-		}).catch(erro => {
-			console.error(erro);
-			res.status(500).send(erro.message);
-		});
-  
+    const instrucaoSql = `select top 1 temperatura, umidade, FORMAT(momento,'HH:mm:ss') as momento_grafico  
+						from Eventos order by id_evento desc`;
+
+    sequelize.query(instrucaoSql, { type: sequelize.QueryTypes.SELECT })
+        .then(resultado => {
+            res.json(resultado[0]);
+        }).catch(erro => {
+            console.error(erro);
+            res.status(500).send(erro.message);
+        });
+
 });
 
 
 // estatísticas (max, min, média, mediana, quartis etc)
-router.get('/estatisticas', function (req, res, next) {
-	
-	console.log(`Recuperando as estatísticas atuais`);
+router.get('/estatisticas', function(req, res, next) {
 
-	const instrucaoSql = `select 
-							max(Temperatura) as temp_maxima, 
-							min(Temperatura) as temp_minima, 
-							avg(Temperatura) as temp_media,
-							max(Umidade) as umidade_maxima, 
-							min(Umidade) as umidade_minima, 
-							avg(Umidade) as umidade_media 
+    console.log(`Recuperando as estatísticas atuais`);
+
+    const instrucaoSql = `select 
+							max(temperatura) as temp_maxima, 
+							min(temperatura) as temp_minima, 
+							avg(temperatura) as temp_media,
+							max(umidade) as umidade_maxima, 
+							min(umidade) as umidade_minima, 
+							avg(umidade) as umidade_media 
 						from Eventos`;
 
-	sequelize.query(instrucaoSql, { type: sequelize.QueryTypes.SELECT })
-		.then(resultado => {
-			res.json(resultado[0]);
-		}).catch(erro => {
-			console.error(erro);
-			res.status(500).send(erro.message);
-		});
-  
+    sequelize.query(instrucaoSql, { type: sequelize.QueryTypes.SELECT })
+        .then(resultado => {
+            res.json(resultado[0]);
+        }).catch(erro => {
+            console.error(erro);
+            res.status(500).send(erro.message);
+        });
+
 });
 
 
